7617f570720a181d034df3c989e12d8c90e96b03
YtalloLima <ytalloaugusto@gmail.com>
2020-02-05 16:28:47 -0300

    Instalando e configurando o devise

--- basico_projeto/config/routes.rb
+++ basico_projeto/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  devise_for :members
   namespace :site do
     get 'home', to:'home#index'
   end
   namespace :backoffice do
-    get 'dashboard' to:'dashboard#index' # quando digitar dashboard, irá para o controle dashboard ação index
+    get 'dashboard', to:'dashboard#index' # quando digitar dashboard, irá para o controle dashboard ação index
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
